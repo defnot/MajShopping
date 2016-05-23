4aa8f5fe818fed4d1a560414783c8a97403e13ef
unknown <dimitrov829@gmail.com>
2016-05-23 12:23:05 +0300

    Adding a POJO for the list items

---
+++ app/src/main/java/com/dany/majesticshopping/model/ListItem.java
@@ -0,0 +1,8 @@
+package com.dany.majesticshopping.model;
+
+/**
+ * Created by Dany on 5/23/2016.
+ */
+public class ListItem {
+    // here we are gonna create a POJO(plain java object) for the items of the list
+}
