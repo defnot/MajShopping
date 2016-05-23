ae9e0bf40e849112a14782d66f805e2e374e15bf
unknown <dimitrov829@gmail.com>
2016-05-23 12:28:02 +0300

    Created the item lists POJO

--- app/src/main/java/com/dany/majesticshopping/model/ListItem.java
+++ app/src/main/java/com/dany/majesticshopping/model/ListItem.java
@@ -4,5 +4,20 @@ package com.dany.majesticshopping.model;
  * Created by Dany on 5/23/2016.
  */
 public class ListItem {
-    // here we are gonna create a POJO(plain java object) for the items of the list
+    private String name;
+    private String owner;
+
+    public ListItem(String name) {
+        this.name = name;
+        this.owner = "Anonymous owner";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
 }
