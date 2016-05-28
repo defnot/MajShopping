5e6a134f90cfd8c8f3d9b7cf97e0a84e7e315a02
unknown <dimitrov829@gmail.com>
2016-05-28 13:32:55 +0300

    Made the login activity as the starting one

--- app/src/main/java/com/dany/majesticshopping/ui/login/CreateAccountActivity.java
+++ app/src/main/java/com/dany/majesticshopping/ui/login/CreateAccountActivity.java
@@ -6,6 +6,7 @@ package com.dany.majesticshopping.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -14,11 +15,18 @@ import android.widget.Toast;
 
 import com.dany.majesticshopping.R;
 import com.dany.majesticshopping.ui.BaseActivity;
+import com.dany.majesticshopping.utils.Constants;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
 
 public class CreateAccountActivity extends BaseActivity {
     private static final String LOG_TAG = CreateAccountActivity.class.getSimpleName();
     private ProgressDialog mAuthProgressDialog;
+    private Firebase mFirebaseRef;
     private EditText mEditTextUsernameCreate, mEditTextEmailCreate, mEditTextPasswordCreate;
+    private String mUserName, mUserEmail, mUserPassword;
 
 
     @Override
@@ -26,26 +34,16 @@ public class CreateAccountActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        /**
-         * Link layout elements from XML and setup the progress dialog
-         */
+        mFirebaseRef = new Firebase(Constants.UNIQUE_FIREBASE_URL);
+
         initializeScreen();
     }
 
-    /**
-     * Override onCreateOptionsMenu to inflate nothing
-     *
-     * @param menu The menu with which nothing will happen
-     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
 
-
-    /**
-     * Link layout elements from XML and setup the progress dialog
-     */
     public void initializeScreen() {
         mEditTextUsernameCreate = (EditText) findViewById(R.id.edit_text_username_create);
         mEditTextEmailCreate = (EditText) findViewById(R.id.edit_text_email_create);
@@ -74,7 +72,50 @@ public class CreateAccountActivity extends BaseActivity {
      * Create new account using Firebase email/password provider
      */
     public void onCreateAccountPressed(View view) {
+        mUserName = mEditTextUsernameCreate.getText().toString();
+        mUserEmail = mEditTextEmailCreate.getText().toString().toLowerCase();
+        mUserPassword = mEditTextPasswordCreate.getText().toString();
 
+        /**
+         * Check that email and user name are okay
+         */
+        boolean validEmail = isEmailValid(mUserEmail);
+        boolean validUserName = isUserNameValid(mUserName);
+        boolean validPassword = isPasswordValid(mUserPassword);
+        if (!validEmail || !validUserName || !validPassword) return;
+
+        /**
+         * If everything was valid show the progress dialog to indicate that
+         * account creation has started
+         */
+        mAuthProgressDialog.show();
+
+        /**
+         * Create new user with specified email and password
+         */
+        mFirebaseRef.createUser(mUserEmail, mUserPassword, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                /* Dismiss the progress dialog */
+                mAuthProgressDialog.dismiss();
+                Log.i(LOG_TAG, getString(R.string.log_message_auth_successful));
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                /* Error occurred, log the error and dismiss the progress dialog */
+                Log.d(LOG_TAG, getString(R.string.log_error_occurred) +
+                        firebaseError);
+                mAuthProgressDialog.dismiss();
+                /* Display the appropriate error message */
+                if (firebaseError.getCode() == FirebaseError.EMAIL_TAKEN) {
+                    mEditTextEmailCreate.setError(getString(R.string.error_email_taken));
+                } else {
+                    showErrorToast(firebaseError.getMessage());
+                }
+
+            }
+        });
     }
 
     /**
@@ -84,14 +125,29 @@ public class CreateAccountActivity extends BaseActivity {
     }
 
     private boolean isEmailValid(String email) {
-        return true;
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEditTextEmailCreate.setError(String.format(getString(R.string.error_invalid_email_not_valid),
+                    email));
+            return false;
+        }
+        return isGoodEmail;
     }
 
     private boolean isUserNameValid(String userName) {
+        if (userName.equals("")) {
+            mEditTextUsernameCreate.setError(getResources().getString(R.string.error_cannot_be_empty));
+            return false;
+        }
         return true;
     }
 
     private boolean isPasswordValid(String password) {
+        if (password.length() < 6) {
+            mEditTextPasswordCreate.setError(getResources().getString(R.string.error_invalid_password_not_valid));
+            return false;
+        }
         return true;
     }
 
